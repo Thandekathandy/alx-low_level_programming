83782448efee1dcf0df031259f7c54531b6824f7
Thandekathandy <thandekathandymvelase@gmail.com>
2023-03-20 17:27:16 -0700

    5-printf.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main - A program that prints the size of various computer types
+*Return 0 (Success)
+*/
+int main(void)
+(
+char a;
+int b;
+long int c;
+long long d;
+float f;
+
+printf("Size of char: %lu byte(s)\n,(unsigned long)
+sizeof(a));
